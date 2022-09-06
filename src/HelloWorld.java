bc7c38ca7e6b265bb18b258a982b5be6a9cbe819
Abhishek Bornak <starsabhishek@gmail.com>
2022-09-06 18:23:25 -0500

    Another change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("\tHello World\nHello");
-        //this is comment
-
-        int x = 29;
-        double y = 3.14;
-        boolean z = true;
-        char symbol = '@';
-        String name = "Bro";
-        System.out.println(x);
-        String a = "not me";
-        String b = "who is there?";
-        System.out.println(b+" " + a);
+//        //this is comment
+//
+//        int x = 29;
+//        double y = 3.14;
+//        boolean z = true;
+//        char symbol = '@';
+//        String name = "Bro";
+//        System.out.println(x);
+//        String a = "not me";
+//        String b = "who is there?";
+//        System.out.println(b+" " + a);
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello"+ name);
 
     }
 }
