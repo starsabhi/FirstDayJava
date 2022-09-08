68c4770a5f72d6f1901c392e268759f3b244e110
Abhishek Bornak <starsabhishek@gmail.com>
2022-09-07 19:02:33 -0500

    Another change2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,22 +1,13 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import  java.util.Random;
 public class HelloWorld {
     public static void main(String[] args){
-    double x;
-    double y;
-    double z;
-    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
 
-    System.out.print("Enter side x: ");
-    x = scanner.nextDouble();
 
-    System.out.println("Enter side y: ");
-    y = scanner.nextDouble();
 
-    z = Math.sqrt((x*x)+(y*y));
 
-    System.out.println("The hypotenuse is :"+z);
-    scanner.close();
     }
 }
 
@@ -53,3 +44,21 @@ public class HelloWorld {
 //        System.out.println("Give number");
 //        int age = scanner.nextInt();
 //        System.out.println("Your number "+age);
+
+
+//    public static void main(String[] args){
+//        double x;
+//        double y;
+//        double z;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter side x: ");
+//        x = scanner.nextDouble();
+//
+//        System.out.println("Enter side y: ");
+//        y = scanner.nextDouble();
+//
+//        z = Math.sqrt((x*x)+(y*y));
+//
+//        System.out.println("The hypotenuse is :"+z);
+//        scanner.close();
