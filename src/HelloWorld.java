15bfedc6b26b9f89bc8f0a717dbe556bcb294ff4
Abhishek Bornak <starsabhishek@gmail.com>
2022-09-07 19:04:30 -0500

    Another change2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,8 +5,8 @@ public class HelloWorld {
     public static void main(String[] args){
     Random random = new Random();
 
-
-
+    int x = random.nextInt(7);
+    System.out.println(x);
 
     }
 }
